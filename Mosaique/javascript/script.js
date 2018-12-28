0496f57515bebdf9ab8cac873f00256b85e05af0
maxime9551 <46224983+maxime9551@users.noreply.github.com>
2018-12-28 16:51:50 +0100

    Add files via upload

---
+++ Mosaique/javascript/script.js
@@ -0,0 +1,10 @@
+function onClick(element) {
+	  		document.getElementById("videoid").src = element.src;
+	  		document.getElementById("modal01").style.display = "block";
+		}
+
+		function getImages(channel) {
+			return "./images et vidéo/" + channel + ".jpg" ;
+		}
+
+	
